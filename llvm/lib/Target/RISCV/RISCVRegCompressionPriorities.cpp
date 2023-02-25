ed6e67d40e34091168dd1568cceb9be3f031bbe5
abrasumente <345396594@qq.com>
2023-02-25 20:39:08 +0800

    actually analyse the compression priority

--- llvm/lib/Target/RISCV/RISCVRegCompressionPriorities.cpp
+++ llvm/lib/Target/RISCV/RISCVRegCompressionPriorities.cpp
@@ -11,6 +11,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "RISCV.h"
+#include "RISCVSubtarget.h"
 #include "llvm/ADT/IndexedMap.h"
 #include "llvm/CodeGen/MachineFunctionPass.h"
 #include "llvm/CodeGen/MachineRegisterInfo.h"
@@ -34,8 +35,6 @@ private:
 public:
   static char ID;
 
-  MachineFunction *MF = nullptr;
-
   RISCVRegCompressionPriorities() : MachineFunctionPass(ID) {
     initializeRISCVRegCompressionPrioritiesPass(
         *PassRegistry::getPassRegistry());
@@ -52,25 +51,33 @@ public:
     if (skipFunction(MF.getFunction()))
       return false;
 
+    const auto &STI = MF.getSubtarget<RISCVSubtarget>();
+
     Virt2PriorityMap.clear();
     unsigned NumRegs = MF.getRegInfo().getNumVirtRegs();
     Virt2PriorityMap.resize(NumRegs);
 
     for (auto &MBB : MF) {
       for (auto &MI : MBB) {
-        for (unsigned OpIdx = 0, E = MI.getNumOperands(); OpIdx < E; ++OpIdx) {
-          auto Op = MI.getOperand(OpIdx);
-          if (!Op.isReg())
-            continue;
-          auto RegOp = Op.getReg();
-          if (RegOp.isVirtual()) {
-            // FIXME: Now we only count the number of times a virtual register
-            // appears in ALL instructions.
-            errs() << "This reg is virtual: " << printReg(RegOp) << "\n";
-            Virt2PriorityMap[RegOp]++;
-          } else {
-            errs() << "This reg is not virtual: " << printReg(RegOp) << "\n";
-          }
+
+        auto CompressibleRegs = RISCVRVC::getCompressibleRegs(MI, STI);
+
+        if (CompressibleRegs.empty())
+          continue;
+
+        LLVM_DEBUG(MI.print(dbgs()));
+        LLVM_DEBUG(
+            dbgs() << "this instruction can be compressed, and it has the "
+                      "following compressible registers: ");
+        for (const auto &Reg : CompressibleRegs) {
+          LLVM_DEBUG(dbgs() << printReg(Reg) << ", ");
+        }
+        LLVM_DEBUG(dbgs() << "\n");
+
+        for (const auto &Reg : CompressibleRegs) {
+          assert(Reg.isVirtual() &&
+                 "I thought all registers you returned are virtual?");
+          Virt2PriorityMap[Reg]++;
         }
       }
     }
